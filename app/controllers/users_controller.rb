e51986c33c6e662e99aff021641687adee4d28da
OmoriRiku <oomori3321@gmail.com>
2023-02-22 09:10:22 +0000

    [Finish]Userの詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @book_show = @user.books
   end
 
   def edit
